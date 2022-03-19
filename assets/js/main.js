b35103615898ffdedceae3bb81235136601b5b66
anIcedAntFA <anicedfaant@gmail.com>
2022-03-19 17:02:19 +0700

    build Header, Responsive Mobile Header

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function() {
+    
+    if (header.classList.contains('open')) { // Close Hamburger Menu
+        body.classList.remove('no-scroll');
+        header.classList.remove('open');
+        fadeElements.forEach(function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+
+    } else { // Open Hamburger Menu
+        header.classList.add('open');
+        body.classList.add('no-scroll');
+        fadeElements.forEach(function(element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
